3f351bea0032544c3f83a6fba5dbb6229782d6e2
Michael Barlow <mbarlow@rmi.org>
2025-11-03 22:28:42 -0700

    feat(resources.app.services): add ResourceService with create_resource operation

---
+++ packages/stitch-core/src/stitch/core/resources/app/services/resource_service.py
@@ -0,0 +1,40 @@
+from abc import ABC
+from typing import Any, Mapping
+
+
+from stitch.core.resources.app.mapping import source_record_to_resource_data
+from stitch.core.resources.domain.ports import (
+    TransactionContext,
+)
+
+
+class AbstractService(ABC):
+    tx: TransactionContext
+
+    def __init__(self, tx_ctx: TransactionContext) -> None:
+        self.tx = tx_ctx
+
+
+class ResourceService(AbstractService):
+    def __init__(
+        self,
+        tx_ctx: TransactionContext,
+    ):
+        super().__init__(tx_ctx=tx_ctx)
+
+    # TODO: consider a decorator to wrap methods in a TransactionContext
+    def create_resource(
+        self, source: str, data: Mapping[str, Any], resource_id: int | None = None
+    ):
+        with self.tx:
+            src_repo = self.tx.source_registry.get_source_repository(source)
+            rec_data = src_repo.row_to_record_data(data)
+            source_id = src_repo.write(rec_data)
+            resource_kwargs = source_record_to_resource_data(
+                dataset=source, record=rec_data, source_pk=source_id
+            )
+            resource_id = self.tx.resources.create(**resource_kwargs)
+            _member_id = self.tx.memberships.create(
+                resource_id=resource_id, source_name=source, source_id=source_id
+            )
+            return resource_id
