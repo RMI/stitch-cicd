0b82a91ed2722528c8c563b117d285268d33f0d6
Michael Barlow <mbarlow@rmi.org>
2025-12-11 15:27:15 -0700

    feat(api): add initial resource and health api routes and route prefix

--- deployments/api/src/stitch/api/routers/resources.py
+++ deployments/api/src/stitch/api/routers/resources.py
@@ -1,11 +1,7 @@
 from collections.abc import Sequence
-from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 
-from stitch.core.resources.app.services.resource_service import ResourceService
-from stitch.core.resources.domain.entities import AggregateResourceEntity
-
-from stitch.api.dependencies import get_resource_service
 
 router = APIRouter(
     prefix="/resources",
@@ -13,8 +9,34 @@ router = APIRouter(
 )
 
 
+class ResourceBase(BaseModel):
+    name: str | None
+    country: str | None
+
+
+class Resource(ResourceBase):
+    id: int
+
+
+_resources: list[Resource] = []
+
+
 @router.get("/")
-async def get_all_resources(
-    res_svc: Annotated[ResourceService, Depends(get_resource_service)],
-) -> Sequence[AggregateResourceEntity]:
-    return list(res_svc.get_all_resources())
+async def get_all_resources() -> Sequence[Resource]:
+    return [r for r in _resources]
+
+
+@router.get("/{id}", response_model=Resource)
+async def get_resource(id: int) -> Resource:
+    for r in _resources:
+        if r.id == id:
+            return r
+    raise HTTPException(status_code=404, detail=f"Resource (id: {id}) not found")
+
+
+@router.post("/", response_model=Resource)
+async def create_resource(*, resource_in: ResourceBase) -> Resource:
+    id_ = len(_resources) + 1
+    res = Resource(id=id_, name=resource_in.name, country=resource_in.country)
+    _resources.append(res)
+    return res
