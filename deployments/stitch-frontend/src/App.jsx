3a1333b962371dcb08d4a3fdd7791e94fcba6b19
jasonbrownrmi <122411427+jasonbrownrmi@users.noreply.github.com>
2026-01-14 15:09:17 -0700

    feat(ui): add single resource view to app
    - user can select or enter ID value in input field
    - clicking 'Fetch' button will call the resources endpoint for the selected ID
    - UI render a detail view showing all the attributes (in JSON) returned by the API
    - if a user fetches a non-existent resource, UI displays "Not Found"
    - if the API is unreachable, UI displays error message
    - add Input component
    - add JsonView component
    - add ResourceView component
    - add ResourceView to App
    
    re: STIT-286 - Front-end consumes API and displays Resource detail view

--- deployments/stitch-frontend/src/App.jsx
+++ deployments/stitch-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import ResourcesView from "./components/ResourcesView";
+import ResourceView from "./components/ResourceView";
 
 function App() {
 
   return (
     <div className="min-h-screen w-screen bg-gray-100 p-8">
-      <ResourcesView endpoint="/api/v1/resources/" />
+      <ResourcesView endpoint="/api/v1/resources" />
+      <ResourceView className="mt-24" endpoint="/api/v1/resources/{id}" />
     </div>
   );
 }
